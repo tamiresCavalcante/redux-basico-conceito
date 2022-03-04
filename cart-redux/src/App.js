9236639bc5004d299eaa8f4a718cbcb4ba670fd7
Tamires Cavalcante <tmires.cavalcante@gmail.com>
2022-03-04 19:16:27 -0300

    shopping cart redux

---
+++ cart-redux/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Auth from "./components/Auth";
+import Layout from "./components/Layout";
+
+function App() {
+  return (
+    <div className="App">
+      <Auth />
+    </div>
+  );
+}
+
+export default App;
