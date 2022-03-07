062225f0ac2e13b104f428dc1c486d7266bb9121
Tamires Cavalcante <tmires.cavalcante@gmail.com>
2022-03-07 16:05:56 -0300

    shopping cart redux

--- cart-redux/src/App.js
+++ cart-redux/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 
 function App() {
+
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  console.log(isLoggedIn);
+
+  const cartItems = useSelector((state) => state.cart.itemsList);
+  console.log(cartItems);
   return (
     <div className="App">
-      <Auth />
+      {!isLoggedIn && <Auth />}
+      {isLoggedIn && <Layout />}
     </div>
   );
 }
